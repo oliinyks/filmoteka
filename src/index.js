6f9cc435640b3ceb63a85193088b30015f203cac
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-09-11 12:23:26 +0300

    Add day

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './js/fetchFilms';
 import './js/renderTrends'
 import './js/buildTrends'
-import './js/open-model';
 import './js/renderLocalStorage';
 import './js/modal-movie-card';
 import './js/open-closeModalCard';
+import './js/switch-day-night';
