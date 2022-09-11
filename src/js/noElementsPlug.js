d0bb140b5e52dff9bccb49051481521473543e1b
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-09-11 20:07:47 +0300

    loader and noElemPlug added

---
+++ src/js/noElementsPlug.js
@@ -0,0 +1,11 @@
+const refs = {
+    noElement: document.querySelector('.no-element')
+}
+
+export function EnableNoElementsPlug() {
+    refs.noElement.classList.remove('.is-hidden');
+}
+
+export function EnableNoElementsPlug() {
+    refs.noElement.classList.add('.is-hidden');
+}
