6ad73ad9fb72b4a83d62672479718f5594c22e30
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2022-09-13 02:37:20 +0300

    1

--- src/js/key-url.js
+++ src/js/key-url.js
@@ -1,10 +1,3 @@
 export { API_URL, API_KEY };
 const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '70a1ccf907025a1a646b674d3a53bd64';
-
-function findTrailer(id) {
-  return fetch(
-    `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`
-  ).then(respons => respons.json());
-}
-export { findTrailer };
