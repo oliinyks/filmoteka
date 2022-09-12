fbb89fe50fbf956012e26c13c663827fcc3cdf47
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-09-12 16:21:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './js/fetchFilms';
 import './js/renderTrends';
 import './js/buildTrends';
 import './js/renderLocalStorage';
-import './js/modal-movie-card';
+// import './js/modal-movie-card';
 import './js/switch-day-night';
 import './js/arrow_top';
