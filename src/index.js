f7f73e1fedf60c3ef1d261840bb2bada8775058a
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-09-12 15:00:04 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import './js/fetchFilms';
-<<<<<<< Updated upstream
 import './js/open-model';
-=======
-import './js/renderTrends';
-import './js/buildTrends';
-import './js/renderLocalStorage';
-import './js/modal-movie-card';
-import './js/switch-day-night';
-import './js/arrow_top';
+// import './js/renderTrends';
+// import './js/buildTrends';
+// import './js/renderLocalStorage';
+// import './js/modal-movie-card';
+// import './js/switch-day-night';
+// import './js/arrow_top';
 import './js/loader';
 import './js/noElementsPlug';
->>>>>>> Stashed changes
