9cbde8fccd83f73eaf280dbdb9f8b4c14d3fa7d4
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2022-09-12 16:33:20 +0300

    dev_modal

--- src/js/modal-movie-card.js
+++ src/js/modal-movie-card.js
@@ -1,5 +1,6 @@
 const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '70a1ccf907025a1a646b674d3a53bd64';
+//
 const openModal = document.querySelector('.gallery__list');
 const closeModalBtn = document.querySelector('[data-modal-close]');
 const backdrop = document.querySelector('.modal-movie-card__js-backdrop');
@@ -11,6 +12,7 @@ function openCardMovie(e) {
   document.body.classList.add('show-modal');
   window.addEventListener('keydown', onEscKeyPress);
   id = Number(e.target.getAttribute('data-id'));
+  document.body.style.overflow = 'hidden';
   modalCreateMark();
 }
 //Запрос по id карточки на которую кликнули и рендерим разметку
@@ -19,18 +21,27 @@ async function modalCreateMark() {
   await fetch(url)
     .then(response => response.json())
     .then(movie => {
+      // Жанры
+      let genre = '';
+      let genres = movie.genres.map(({ name }) => {
+        genre = name;
+      });
       document
         .querySelector('.modal-movie-card')
-        .insertAdjacentHTML('afterbegin', createModal(movie));
+        .insertAdjacentHTML('afterbegin', createModal(movie, genre));
     })
-    .catch(error => console.log(error.message));
+    .catch(error => console.log(error));
 }
 // Разметка
-function createModal(movie) {
+function createModal(movie, genre) {
   modalCard.innerHTML = ``;
   return `
   <div class="modal-movie-card__img-wrapper">
-      <img src="https://www.themoviedb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" class="lazyload" data-id=${movie.id} onerror="this.src='https://ik.imagekit.io/tc8jxffbcvf/default-movie-portrait_EmJUj9Tda5wa.jpg?tr=fo-auto,di-';"/>
+      <img src="https://www.themoviedb.org/t/p/w500${movie.poster_path}" alt="${
+    movie.title
+  }" class="lazyload" data-id=${
+    movie.id
+  } onerror="this.src='https://ik.imagekit.io/tc8jxffbcvf/default-movie-portrait_EmJUj9Tda5wa.jpg?tr=fo-auto,di-';"/>
     </div>
     <div class="modal-movie-card__film-desсriptoin">
       <h2 class="modal-movie-card__film-title">${movie.title}</h2>
@@ -38,14 +49,20 @@ function createModal(movie) {
         <tr>
           <td class="modal-movie-card__table-refs">Vote / Votes</td>
           <td class="modal-movie-card__table-value">
-           <span class="modal-movie-card__item-desc--orange">${movie.vote_average}</span>
+           <span class="modal-movie-card__item-desc--orange">${movie.vote_average.toFixed(
+             1
+           )}</span>
            <span>/</span>
-            <span class="modal-movie-card__item-desc--grey">${movie.vote_count}</span>
+            <span class="modal-movie-card__item-desc--grey">${
+              movie.vote_count
+            }</span>
           </td>
         </tr>
         <tr>
           <td class="modal-movie-card__table-refs">Popularity</td>
-          <td class="modal-movie-card__table-value">${movie.popularity}</td>
+          <td class="modal-movie-card__table-value">${movie.popularity.toFixed(
+            1
+          )}</td>
         </tr>
         <tr>
           <td class="modal-movie-card__table-refs">Original Title</td>
@@ -53,7 +70,7 @@ function createModal(movie) {
         </tr>
         <tr>
           <td class="modal-movie-card__table-refs">Genre</td>
-          <td class="modal-movie-card__table-value">${movie.genres}</td>
+          <td class="modal-movie-card__table-value">${genre}</td>
         </tr>
       </table>
       <p class="modal-movie-card__about">About</p>
@@ -83,6 +100,7 @@ function createModal(movie) {
 function closeCardMovie() {
   document.body.classList.remove('show-modal');
   window.removeEventListener('keydown', onEscKeyPress);
+  document.body.style.overflow = '';
 }
 // Выход по backdrop
 function backdropClickClose(e) {
@@ -96,8 +114,9 @@ function onEscKeyPress(e) {
     closeCardMovie();
   }
 }
-
+//
 openModal.addEventListener('click', openCardMovie);
 closeModalBtn.addEventListener('click', closeCardMovie);
 backdrop.addEventListener('click', backdropClickClose);
-export { openCardMovie, id };
+//
+export { openCardMovie };
