2413db2e29c661447f756f04ed237b4e04049eb4
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2022-09-14 14:26:18 +0300

    1

--- src/js/modal-movie-card-close.js
+++ src/js/modal-movie-card-close.js
@@ -1,25 +1,20 @@
 const closeModalBtn = document.querySelector('[data-modal-movie-card-close]');
 const backdrop = document.querySelector('.modal-movie-card__js-backdrop');
-
-// При выходе забираем класс.
 function closeCardMovie() {
   document.body.classList.remove('show-modal');
   window.removeEventListener('keydown', onEscKeyPress);
   document.body.style.overflow = '';
 }
-// Выход по backdrop
 function backdropClickClose(e) {
   if (e.currentTarget === e.target) {
     closeCardMovie();
   }
 }
-// Выход по ESC
 function onEscKeyPress(e) {
   if (e.code === 'Escape') {
     closeCardMovie();
   }
 }
-
 closeModalBtn.addEventListener('click', closeCardMovie);
 backdrop.addEventListener('click', backdropClickClose);
 export { closeCardMovie, onEscKeyPress };
